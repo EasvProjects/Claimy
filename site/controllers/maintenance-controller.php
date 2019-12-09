6fa75926d5f894445c0f292a83bdfea2e2f701e3
wisamswaidan <swaiidan@innovium.dk>
2019-12-09 11:15:25 +0100

    index.php new

--- site/controllers/maintenance-controller.php
+++ site/controllers/maintenance-controller.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require '../view/under-maintenance.php';
+require 'templates/index-template.php';
 $input = 'KimLangholz';
 // Store the cipher method
 $ciphering = "AES-128-CTR";
