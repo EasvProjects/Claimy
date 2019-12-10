a9fda53c4c9076a6bf46ffaa94d21eb8d771bbd7
wisamswaidan <swaiidan@innovium.dk>
2019-12-10 22:40:45 +0100

    dashboard controllers

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,12 +4,3 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require '../dashboard/view/signin.php';
-//require 'site/controllers/';
-
-
-
-// Comment these lines to hide errors
-/*error_reporting(E_ALL);
-ini_set('display_errors', 1);
-require 'includes/config.php';
-require 'includes/functions.php';*/
---
+++ dashboard/view/inc/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer text-center text-md-left">
+
+    <div class="footer-bottom py-2 py-md-3">
+        <div class="container">
+            <div class="copyright font-medium ">Claimy.dk - <span class="address">Address</span> - <span>Cvr. no</span> - <span><a href="#">e-mail</a></span> - @ Copyright</div>
+        </div>
+    </div>
+</footer>
